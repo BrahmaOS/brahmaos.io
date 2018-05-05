106f571531fafd530053dfbd072e4ce0f61cffec
liuning <liuning_nb@163.com>
2018-05-05 16:49:14 +0800

    all

--- resources/views/site2/index.blade.php
+++ resources/views/site2/index.blade.php
@@ -626,7 +626,7 @@
                         community to be distributed for milestones reached and D-Apps build</p>
                 </div>
 
-                <img src="/images/site2/token-Distribution/tokendistribution.png" alt="">
+                <img src="/images/site2/tokenDistribution/tokendistribution.png" alt="">
 
                 <div class="distribution-item developer-community">
                     <div class="left">
