89863f723a00b869da07db92e7ba70f5ee595f81
martin <wangqingfei1014@gmail.com>
2018-05-15 17:51:42 +0800

    Change online whitepage two language to Chinese.

--- resources/views/site2/abstract2zh.blade.php
+++ resources/views/site2/abstract2zh.blade.php
@@ -3,186 +3,196 @@
 @section("docs-content")
 <div>
     <!-- Layered Design Architecture -->
-    <h1 id="Architecture">Architecture</h1>
+    <h1 id="Architecture">架构</h1>
 
     <br>
-    <h3 id="LayeredDesign">Layered Design</h3>
+    <h3 id="LayeredDesign">分层设计</h3>
 
     <img src="/images/site2/9.jpg" alt="">
 
     <p>
-        Brahma OS uses a layered design scheme to stratify Chain, Runtime, and Framework.
-        Taking into account the Android native application compatibility issues,
-        we separate the Virtual Machine so that Android App runtime environment and DApp runtime environment are entirely isolated and to ensure that,
-        even if there is dangerous behavior, Android App cannot get DApp information.
+        Brahma OS  使用分层设计方案，将 Chain、Runtime、Framework
+        等逐一分层。并且为了考虑到Android  原生应用的兼容性问题，从
+        Virtual Machine  单独区分，从而使得 Android App 和 DApp  的运
+        行时环境完全隔离，保证了 Android App  即便存在危险的行为也不
+        能获取到 DApp  的信息。
     </p>
 
     <p>
-        Brahma Virtual Machine (BVM) is specially designed for DApp in Brahma OS to run DApp and the associated Framework and Libraries.
+        Brahma OS  为 DApp 独立设计了 Brahma Virtual Machine(BVM)，
+        用于执行 DApp  的运行和关联的 Framework 和 Libraries  的运行。
     </p>
 
     <p>
-        Brahma's main chain as a Chain Context is expected to serve as an intermediary role to connect other application chains (Ethereum, EOS, etc.) through Chain Relayer.
+        Brahma  独立的 Chain 作为 Brahma OS  的主链，起着  Chain
+        Context(链间上下文)的作用，预期通过 Chain Relayer  作为中间角
+        色，来连通其他的应用链（如 Ethereum，EOS  等）。
     </p>
 
 
     <br>
     <!-- Consensus algorithm -->
-    <h3 id="Consensusalgorithm">Consensus algorithm</h3>
+    <h3 id="Consensusalgorithm">共识算法</h3>
     <p>
-        As an independent blockchain, Brahma OS Chain's consensus algorithm is crucial to security.
-        Currently, we mainly consider two possible projects: DPoS and DAG.
+        作为独立的区块链，Brahma OS Chain  的共识算法对于安全性至关
+        重要，当前我们主要考虑采用两种可能的方案：DPoS  和 DAG。
     </p>
 
     <p>
-        DPoS was originally proposed by Bytemaster and used in BitShares, Steemit and EOS.
-        Its main features are higher TPS (TX per second) and lower consensus maintenance costs.
-        In contrast to the PoW consensus competition mechanism,
-        DPoS tend to reach a consensus with the collaboration of Witness node.
+        DPoS  由 Bytemaster 最初提出并在 BitShares、Steemit  及  EOS
+        中应用，它主要的特点是较高的 TPS (TX per second)  和较低的共
+        识维护成本，同时相比于 PoW 共识的竞争机制，DPoS  更倾向于
+        Witness node  间合作的方式来达成共识。
     </p>
 
     <p>
-        DAG is a graph in graph theory. In the beginning,
-        IOTA attempted to use DAG instead of the existing blockchain structure
-        to provide a value delivery network by mathematical proof rather than by consensus mechanism.
-        After IOTA, there are other projects such as Byteball DAG proposed improvements and applications.
-        The main feature of DAGs is the ability to deal with transaction requests at higher concurrency
-        because of no block structure and therefore, no limit to TPS.
+        DAG  是图论中的一种图，IOTA 最初尝试使用 DAG  来替代已有的
+        blockchain  结构，用于提供一种通过数学证明，而非共识机制保证
+        的价值传输网络。在  IOTA
+        之后，又有  Byteball
+        等其他项目对
+        DAG  提出了改进和应用。DAG 的主要特点在于可以更高并发的处
+        理交易请求，因为没有了 block  结构，因此不存在 TPS 的限制。
     </p>
 
 
     <br>
     <!-- Compatibility and Security -->
-    <h3 id="CompatibilityandSecurity"> Compatibility and Security</h3>
+    <h3 id="CompatibilityandSecurity"> 兼容性与安全性考虑</h3>
     <p>
-        We believe that decentralized OS will eventually replace the current centralized OS.
-        This process requires not only time and effort but also learning of users.
-        At the beginning of the design, we would take both compatibility and security into consideration.
+        我们相信去中心化 OS  最终会替换掉当前的中心化 OS，但是这不仅
+        仅需要时间和精力，还需要用户学习的过程，因此我们在设计之初，
+        会坚固兼容性并考虑到安全性因素。
     </p>
 
     <p>
-        The existing Android OS is relatively complete. Therefore we consider
-        providing a runtime environment with Dalvik Virtual Machine for the existing
-        App of Android while we work on the related products of Brahma OS (such as Brahma Virtual Machine and Brahma Chain).
+        因为已有的  Android
+        OS
+        已经相对完整，因此我们考虑在进行
+        Brahma OS  相关内容研发的同时，如Brahma Virtual Machine、
+        Brahma Chain  等，兼顾已有 Android OS  的运行，在 OS  层面提供
+        Dalvik Virtual Machine  为 Android 已有的 App 提供运行时环境。
     </p>
 
     <p>
-        The end nodes in the meta-network can form a peer-to-peer network,
-        that is, the nodes in any meta-network are offline and should not cause unreachable communication in the meta-network.
-        This is not the core of our concern, as its connectivity problems are not hard to solve.
-        The critical problem goes to the router.
-    </p>
-
-    <p>
-        In this way, it guarantees the compatibility.
-        At the same time, it isolates the interaction between the Android App and the DApp from the runtime level,
-        thereby avoiding the possibility that the Android App can steal the key and information in the DApp.
+        这样，既保证了兼容性，又从运行时的层面直接隔离了  Android
+        App 与 DApp  之间交互的可能性，从而避免了 Android App  窃取密
+        钥、偷取 DApp  信息的可能性。
     </p>
 
     <br>
 
     <!--Identity-->
-    <h1 id="Identity"> Identity</h1>
+    <h1 id="Identity"> 身份</h1>
     <p>
 
-        Identity is the primary element in any system and the premise of
-        logical interaction and functional interaction between man and machine.
+        在任何系统中，身份都是不可规避的首要因素，也是人机进行逻辑、
+        功能性交互的前提。
     </p>
     <p>
-        In Brahma OS, the identity is no longer dependent on the centralized service account system,
-        but directly use the blockchain public key as the identity of the user identity,
-        connecting the entire decentralized service system.
+        Brahma OS  中，身份不再依赖于基于中心化服务的账号系统，而是
+        直接使用区块链的公钥作为用户身份的标识，连接整个去中心化服务
+        体系。
     </p>
 
 
     <br>
     <!-- About  -->
-    <h3 id="AboutI">About "I"</h3>
+    <h3 id="AboutI">关于“我”</h3>
     <p>
-        The reason why we will abandon the centralized service account system,
-        largely from our understanding of the identity of "I" in the entire network architecture.
+        之所以会抛弃中心化服务的账号体系，很大程度上来源于我们对于
+        “我”这一身份在整个网络架构中的认知。
     </p>
 
     <p>
-        In the existing centralized account system, end users need to register with a centralized service provider to get a distributed identity.
-        This identity authorizes the user to log in to the system for the follow-up operation. This brings a few questions:
+        在现在已有的中心化账号体系中，终端用户需要在中心化的服务商那
+        里进行注册，拿到一个分发的身份，授权用户登录到系统中，再进行
+        后续操作。这带来几个问题（现在已经凸显）：
     </p>
 
     <ul>
-        <li class="li_disc">When switching between different account systems, the data of "I" do not interwork with each other</li>
-        <li class="li_disc">"I" is redundant in different accounts and not mutually exclusive</li>
-        <li class="li_disc">All factual data and user from the "I" act are easily obtained at no cost by a centralized service provider</li>
+        <li class="li_disc">在不同的账号系统间切换时，“我”的数据并不天然互通</li>
+        <li class="li_disc">“我”在不同账号里是冗余且并不互备的</li>
+        <li class="li_disc">“我”的行为产生的所有事实数据和用户画像被中心化的服务商无成本轻松获取</li>
     </ul>
 
     <p>
-        In summary, "I" is authorized to access in the existing centralized network
-        and is unconditionally deprived of data.
+        以上几个问题总结来说就是，“我”在当前已有的中心化网络中是被授权访问，且无条件的在被剥夺行为数据。
     </p>
     <p>
-        When an account leaked the password, the service provider was hacked or service providers sell user personal data, "I" becomes powerless.
+        当一个账号泄漏了密码或服务商被黑客攻击，甚至是服务商贩卖用户个人数据时，“我”便变得对这件事情无能为力。
     </p>
 
     <br>
     <!--  Role and Value of Identity -->
-    <h3 id=" RoleandValueofIdentity">Role and Value of Identity</h3>
+    <h3 id=" RoleandValueofIdentity">身份的作用与价值体现</h3>
     <p>
-        Based on our perception of "I", i.e. identity, we can find that identity, as an access token to the system,
-        is linked to a set of business logic.The identities built on the central service providers are isolated, redundant, non- cross-linked and insecure. However, user's identity is a valuable resource and barrier in the current Internet economy. Therefore, returning the value of identity to the user is paramount for Brahma OS.
+        基于对于“我”，即身份，的认知我们不难发现，身份作为接入到系统
+        的准入标识，背后关联着一整套的商业逻辑。
+        而构建在中心服务商之上的身份，彼此之间是隔离、冗余、无交叉、
+        不安全的，而用户身份又恰恰是当前互联网经济的重要资源和壁垒。
+        所以，在 Brahma OS  中，如何将身份的价值回归给身份所有者本身
+        便显得至关重要了。
     </p>
 
     <p>
-        Through the above description, we can see that identity in a complete system plays two roles:
-    </p>
-
-    <ul>
-        <li class="li_disc">Get Account System Authorization</li>
-        <li class="li_disc">Access to and operation of the account system's identity</li>
-    </ul>
-
-    <p>
-        In Brahma OS, identities plays other roles:
+        通过上面的阐述，可以知道，身份在一个完备的系统中起着两种作用：
     </p>
 
     <ul>
-        <li class="li_disc">Asset identification</li>
-        <li class="li_disc">AUser portrait</li>
-        <li class="li_disc">User portrait D-App matches</li>
+        <li class="li_disc">获取账户系统授权</li>
+        <li class="li_disc">访问和操作账户系统的标识</li>
     </ul>
 
     <p>
-        Brahma OS expects to offer users the opportunity to collect user factual data at the OS level and to train their AI for perfection of user portraits.
-        When D-App needs to use these user portraits, it pays directly to the user to ensure that the final revenue belongs to the owner of the OS, not to any intermediate role.
+        在Brahma OS中，身份还具有其他作用：
+    </p>
+
+    <ul>
+        <li class="li_disc">资产的标识</li>
+        <li class="li_disc">用户画像的标识</li>
+        <li class="li_disc">D-App匹配用户画像的标识</li>
+    </ul>
+
+    <p>
+        Brahma OS  预期提供给用户可选择机会，通过 OS 层面在本地收
+        集用户的事实数据，并训练其本地 AI       进行用户画像的完善，当  D-
+        App  的主体需要使用这些用户画像时，其直接支付费用给用户，确
+        保最终收益归 OS  所有者本人，而非任何中间角色。
     </p>
 
     <br>
     <!--Significance of Decentralized Storage-->
-    <h3 id="SignificanceOfStorage">Significance of Decentralized Storage</h3>
+    <h3 id="SignificanceOfStorage">去中心化存储的意义</h3>
     <p>
-        Decentralized storage of user-related data can be more valuable than other centralized cloud storage.
+        相比于直接使用其他中心化的云存储方案，用户的身份相关数据使用
+        去中心化存储会带来更大的价值。
     </p>
 
     <p>
-        We talk about several disadvantages of centralized cloud storage:
+        先说下使用中心化云存储方案的几个弊端：
     </p>
     <ul>
-        <li class="li_disc">It exists systemic security risks in Centralized systems</li>
-        <li class="li_disc">Cloud storage service providers do not share identity information</li>
-        <li class="li_disc">Any centralized cloud storage service providers compromising will threaten other service providers</li>
+        <li class="li_disc">中心化系统存在系统性安全风险</li>
+        <li class="li_disc">云存储服务商之间并不共享身份信息</li>
+        <li class="li_disc">任何一个中心化云存储服务商被攻破都会威胁其他服务商</li>
     </ul>
 
     <p>
-        At the beginning of designing the decentralized storage, we avoid such kind of problems.
-        Even if the key is stolen due to personal or other reasons,
-        the leakage of personal data will not cause the loss of other user data in the system.
+        而去中心化的存储方案，恰好在设计之初就规避这类问题。即便是单
+        一用户因为个人行为或其他原因导致密钥被盗窃，带来个人数据的泄
+        漏，也不会造成整个系统中其他用户数据的丢失，甚至于都不会产生
+        丝毫的关联。
     </p>
 
     <p>
-        At the same time, user identity is no longer a source for commercial company to get information of users.
-        All fees will go back to the user itself. The problem of requiring centralized storage and processing of user data was circumvented from the very beginning.
-        Everyone holds their own data which distributed discretely in a decentralized storage network.
-        No one (organization) can monitor the whereabouts and content of personal data or hijack user data.
-        This will bring a lot of possibilities for personal data privacy, freedom of behavior and value return.
-        it is the basis for reshaping the existing business model.
+        同时，用户身份不再是被商业巨头榨取的来源。所有因为使用用户身
+        份及画像支付的费用，都会回馈给用户本身。从一开始便规避了用户
+        数据需要集中存储和处理的问题。每个人持有着自己的数据，离散的
+        分布在去中心化存储的网络中，不再有人（机构）可以监视个人数据
+        的去向、内容，也无法在中间劫持数据。
+        这将为个人的数据隐私、行为自由、价值回归带来诸多的可能性，是
+        重塑现有商业模式的基础。
     </p>
 
 </div>
